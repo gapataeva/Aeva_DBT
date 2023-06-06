927f32002130845b15abd806e14b31e3f6f9d91a
gap25@njit.edu <gap25@njit.edu>
2023-06-06 14:03:46 +0000

    developing stg_orders

---
+++ models/stg/stg_orders.sql
@@ -0,0 +1,22 @@
+select 
+-- from raw_order
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+o.ORDERCOSTPRICE,
+o.ORDERSELLPRICE,
+o.ORDERSELLPRICE - o.ORDERCOSTPRICE as profit ,
+-- from raw_customer
+c.customername,
+c.segment,
+c.country,
+-- from raw_product
+p.category,
+p.productname,
+p.subcategory
+from {{ ref('raw_order') }} as o
+left join {{ ref('raw_customer') }} as c
+on o.customerid = c.customerid
+left join {{ ref('raw_product') }} as p
+on o.productid = p.productid
